a6821f86e996828401a83d821a2d0e094c469da7
jugshaurya <shauryasinghal84@gmail.com>
2019-01-22 09:03:14 +0530

    while revising changes

--- Learn Python/test_cap.py
+++ Learn Python/test_cap.py
@@ -1,22 +1,17 @@
-import unittest
-import cap
+import unittest  # to test import unittest
+import cap 
 
-class TestCap(unittest.TestCase):
+class TestCap(unittest.TestCase): # inherit unittest.TestCase
     
-    def test_one_word(self):
+    def test_one_words(self):
         text = 'python'
         result = cap.cap_text(text)
-        self.assertEqual(result, 'Python')
+        self.assertEqual(result, 'Python') # check for equlity using self.assertEqual
         
     def test_multiple_words(self):
         text = 'monty python'
         result = cap.cap_text(text)
         self.assertEqual(result, 'Monty Python')
         
-    def test_with_apostrophes(self):
-        text = "monty python's flying circus"
-        result = cap.cap_text(text)
-        self.assertEqual(result, "Monty Python's Flying Circus")
-        
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main() #run testcases using unittest.main()
